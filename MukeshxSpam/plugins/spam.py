808b1690f4ba31f5322db99e3c096d39b3efb7c5
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2022-08-18 21:15:44 +0530

    Update spam.py

--- MukeshxSpam/plugins/spam.py
+++ MukeshxSpam/plugins/spam.py
@@ -26,7 +26,36 @@ from telethon.tl.types import InputStickerSetID, InputStickerSetShortName
 from telethon import utils
 from resources.data import GROUP, PORMS, LOVESPAM
 
-
+@Mlk.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
+@Mlk2.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
+@Mlk3.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
+@Mlk4.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
+@Mlk5.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
+@Mlk6.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
+@Mlk7.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
+@Mlk8.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
+@Mlk9.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
+@Mlk10.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
+async def lovespam(e):
+    if e.sender_id in SUDO_USERS:
+        if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
+            return await e.reply(usage, parse_mode=None, link_preview=None )
+        rizoel = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
+        if len(rizoel) == 1:
+            counter = int(rizoel[0])
+            if int(e.chat_id) in GROUP:
+                text = f"Sorry !! I can't spam here"
+                await e.reply(text, parse_mode=None, link_preview=None )
+            else:
+                 love = random.choice(LOVESPAM)
+                 for _ in range(counter):
+                     async with e.client.action(e.chat_id, "document"):
+                         smex = await e.client.send_file(e.chat_id, porrn)
+                         await gifspam(e, smex) 
+                     await asyncio.sleep(0.4)
+        else:
+            usage = f"**MODULE NAME : LOVE SPAM** \n\n command: `{hl}lovespam <count>`"
+            await e.reply(usage, parse_mode=None, link_preview=None )
 
 @Mlk.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
 @Mlk2.on(events.NewMessage(incoming=True, pattern=r"\%sspam(?: |$)(.*)" % hl))
@@ -219,38 +248,6 @@ async def hang(e):
                 await asyncio.wait([e.respond(hang, reply_to=e.reply_to_msg_id) for i in range(counter)])
         else:
             await e.reply(usage)
-
-
-@Mlk.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
-@Mlk2.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
-@Mlk3.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
-@Mlk4.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
-@Mlk5.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
-@Mlk6.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
-@Mlk7.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
-@Mlk8.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
-@Mlk9.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
-@Mlk10.on(events.NewMessage(incoming=True, pattern=r"\%lovespam(?: |$)(.*)" % hl))
-async def lovespam(e):
-    if e.sender_id in SUDO_USERS:
-        if e.text[0].isalpha() and e.text[0] in ("/", "#", "@", "!"):
-            return await e.reply(usage, parse_mode=None, link_preview=None )
-        rizoel = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
-        if len(rizoel) == 1:
-            counter = int(rizoel[0])
-            if int(e.chat_id) in GROUP:
-                text = f"Sorry !! I can't spam here"
-                await e.reply(text, parse_mode=None, link_preview=None )
-            else:
-                 love = random.choice(LOVESPAM)
-                 for _ in range(counter):
-                     async with e.client.action(e.chat_id, "document"):
-                         smex = await e.client.send_file(e.chat_id, porrn)
-                         await gifspam(e, smex) 
-                     await asyncio.sleep(0.4)
-        else:
-            usage = f"**MODULE NAME : LOVE SPAM** \n\n command: `{hl}lovespam <count>`"
-            await e.reply(usage, parse_mode=None, link_preview=None )
 
 @Mlk.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
 @Mlk2.on(events.NewMessage(incoming=True, pattern=r"\%spackspam(?: |$)(.*)" % hl))
